671e88687fd996c9083b1522df2d9294d9729726
Misael Assueiro Vergara <misael.vergara@raizen.com>
2025-11-03 17:33:46 -0300

    chore(): adjusts cart icon

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -12,4 +12,9 @@ export class HeaderComponent {
   @Input() title: string = 'Cupcakes gourmet';
   @Input() showCart: boolean = false;
   @Input() cartItemCount: number = 0;
+  @Output() cartClick = new EventEmitter<void>();
+
+  onCartClick(): void {
+    this.cartClick.emit();
+  }
 }
